223378b212bbbb1bd4348c4d912179af14754f99
toshegg <toshegg190494@gmail.com>
2015-11-25 01:59:35 +0600

    Init commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDeusCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "";
+
+            if (args.Contains<string>("-h"))
+            {
+                Console.WriteLine("Type -f 'filename' to enter filename\nType -m 'find' -s 'string' to find byte offset of each entry of 'string' in file\nType -m 'checksum' to find checksum of each 32-bit word in file\nType -h to display info about commands and parameters");
+                return;
+            }
+
+            if (args.Contains<string>("-f"))
+            {
+                filename = args[Array.IndexOf(args, "-f") + 1];
+                if (!File.Exists(filename))
+                {
+                    Console.WriteLine("File {0} doesn't exist", filename);
+                    return;
+                }
+            }
+
+            if (args.Contains<string>("-m"))
+            {
+                var mode = args[Array.IndexOf(args, "-m") + 1];
+                switch (mode) {
+                    case "find":
+                        string str;
+                        if (args.Contains<string>("-s"))
+                        {
+                            str = args[Array.IndexOf(args, "-s") + 1];
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter -s 'string_to_find'");
+                            return;
+                        }
+                        var offsets = ProgramHelper.Find(filename, str);
+                        Console.WriteLine("Offsets: {0}", string.Join(" ", offsets.ToArray().Select(x => x.ToString()).ToArray()) );
+                        break;
+                    case "checksum":
+                        var checksum = ProgramHelper.Checksum(filename);
+                        Console.WriteLine("Checksum: {0}", checksum);
+                        break;
+                    default:
+                        Console.WriteLine("Please, enter 'find' or 'checksum' as mode");
+                        return;
+                }
+            }
+           
+        }
+    }
+}
---
+++ ProgramHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDeusCraft
+{
+    class ProgramHelper
+    {
+        public static long Checksum(string filename)
+        {
+            byte[] data = new byte[4];
+            long checksum = 0;
+            int read_count;
+            using (Stream fs = File.OpenRead(filename))
+            {
+
+                for (int i = 0; i < fs.Length; i += 4)
+                {
+                    read_count = 4;
+
+                    if (i + 4 >= fs.Length)
+                    {
+                        read_count = (int)fs.Length - i;
+                    }
+                    
+                    fs.Read(data, 0, read_count);
+                    checksum += BitConverter.ToInt32(data, 0);
+                }
+                fs.Close();
+            }
+            return checksum;
+        }
+
+        public static List<int> Find(string filename, string str)
+        {
+            int equal_count;
+            List<int> offsets = new List<int>();
+            byte[] byte_str = GetBytes(str);
+            byte[] data;
+
+            using (Stream fs = File.OpenRead(filename))
+            {
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int) fs.Length);
+                fs.Close();
+            }
+
+            data = Array.FindAll(data, b => b != 0);
+            for (int i = 0; i < data.Length; i++)
+            { 
+                if (data[i] == byte_str[0])
+                {
+                    equal_count = 1;
+                    for (int j = 1; j < byte_str.Length && i + j < data.Length; j++)
+                    {
+                        if (data[i + j] == byte_str[j])
+                            equal_count++;
+                        else break;
+                    }
+
+                    if (equal_count == byte_str.Length)
+                        offsets.Add(i);
+                }
+            }
+            return offsets;
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            //this deletes zero bytes. need for letters, which value < 256 in 2-bytes encodings
+            return Array.FindAll(bytes, b => b != 0);
+        }
+
+    }
+}
